b0e42bfabf0e2f0e0b35a8435c011a1f86427c8a
sergio@sergiomini <sergiolindo.empresa@gmail.com>
2013-01-27 19:45:06 +0100

    Change project name in license text: - Antes   -> web-app-visual-apache - Después -> webapp-visual-apache2

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Web application to configure apache2.
 
+<img style="border:0px;" src="https://raw.github.com/binary-sequence/webapp-visual-apache2/master/img/gplv3-127x51.png">
+
+Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
+
+'webapp-visual-apache2' is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+'webapp-visual-apache2' is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+'webapp-visual-apache2'. If not, see [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/).
+
 ### Screenshoot
 
 <img style="border:0px;" src="https://lh5.googleusercontent.com/-mxuF31Pdq6o/UMI7Kyd3-VI/AAAAAAAACM0/vqP5wrKDZqo/s640/webapp-visual-apache2.jpg">
--- php/ajax/deleteHost.php
+++ php/ajax/deleteHost.php
@@ -1,22 +1,22 @@
 <?php
 /*
 
-  This file is part of 'web-app-visual-apache'.
+  This file is part of 'webapp-visual-apache2'.
 
   Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
 
-  'web-app-visual-apache' is free software: you can redistribute it and/or
+  'webapp-visual-apache2' is free software: you can redistribute it and/or
   modify it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or (at your
   option) any later version.
 
-  'web-app-visual-apache' is distributed in the hope that it will be useful,
+  'webapp-visual-apache2' is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
   Public License for more details.
 
   You should have received a copy of the GNU General Public License along with
-  'web-app-visual-apache'. If not, see <http://www.gnu.org/licenses/>.
+  'webapp-visual-apache2'. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
